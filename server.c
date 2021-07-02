64d09a0019c607af6157601f5fc9f0af5d7b9f4d
Aymane ELmadidi <aymanelmadidi@gmail.com>
2021-07-02 12:33:07 +0100

    push

--- server.c
+++ server.c
@@ -5,7 +5,18 @@
 #include <time.h>
 #include <stdio.h>
 
+void	*ft_memset(void *b, int c, size_t n)
+{
+	size_t			i;
 
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char*)b)[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
 int		len(long nb)
 {
 	int		len;
